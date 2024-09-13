d69bc21eced87765f91b5a929841ef5284da3f8d
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-09-12 21:45:12 -0230

    First model

--- review_system/reviews/models.py
+++ review_system/reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    username = models.CharField(max_length=255)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review made by {self.username}, {self.date}"
+    
